306a0629a790c319d8ead71c22296195c22f0eac
krasnayaiva <piki.killer@gmail.com>
2025-11-30 19:36:32 +0300

    Remove sqlite3 dependencies and fix functions

---
+++ netlify/functions/create-tree.js
@@ -0,0 +1,14 @@
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+  
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      message: "Дерево добавлено",
+      id: Date.now()
+    })
+  };
+}
